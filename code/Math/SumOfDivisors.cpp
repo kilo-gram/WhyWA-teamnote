af26e100328e96144a526507d33f564c8140a698
kilo-gram <kilogram81279@gmail.com>
2025-10-09 12:16:45 +0900

    Create SumOfDivisors.cpp

---
+++ code/Math/SumOfDivisors.cpp
@@ -0,0 +1,24 @@
+ll SumOfDivisors(ll num) {
+    ll total = 1;
+
+    for (int i = 2; (ll)i * i <= num; i++) {
+        if (num % i == 0) {
+            int e = 0;
+            do {
+                e++;
+                num /= i;
+            } while (num % i == 0);
+
+            ll sum = 0, pow = 1;
+            do {
+                sum += pow;
+                pow *= i;
+            } while (e-- > 0);
+            total *= sum;
+        }
+    }
+    if (num > 1) {
+        total *= (1 + num);
+    }
+    return total;
+}
