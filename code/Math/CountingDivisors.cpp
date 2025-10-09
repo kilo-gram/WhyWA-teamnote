30a0bcfb2c61d919db2306d778cacc9121f38583
kilo-gram <kilogram81279@gmail.com>
2025-10-09 12:14:55 +0900

    Create CountingDivisors.cpp

---
+++ code/Math/CountingDivisors.cpp
@@ -0,0 +1,18 @@
+// you can restore any Divisors
+ll numberOfDivisors(ll num) {
+    ll total = 1;
+    for (int i = 2; (ll)i * i <= num; i++) {
+        if (num % i == 0) {
+            int e = 0;
+            do {
+                e++;
+                num /= i;
+            } while (num % i == 0);
+            total *= e + 1;
+        }
+    }
+    if (num > 1) {
+        total *= 2;
+    }
+    return total;
+}
